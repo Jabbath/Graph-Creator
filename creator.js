56a20f078974d3f1cae4f8104903552d8123f8c1
Anton Afanassiev <antonafana@yahoo.ca>
2017-05-23 14:16:56 -0230

    Added ability to add edges between nodes.

--- creator.js
+++ creator.js
@@ -1,7 +1,7 @@
 //*******************************************************************
 //NODE MANIPULATION
 //*******************************************************************
-nodes = [];
+var nodes = [];
 
 class node {
 /*
@@ -12,9 +12,25 @@ This is a node object. It store the position and edges of a node.
         this.yPos = yCoord;
         this.adjacencies = [];
     }
+
+    //Checks if an (x, y) coordinate is in the drawn node
+    inNode(xPos, yPos){
+        const MAX_RADIUS = 10;
+        
+        //Calculate the radius of the click from the circle's center
+        var radius = Math.sqrt(Math.pow(xPos - nodes[i].xPos, 2) + 
+        Math.pow(yPos - nodes[i].yPos, 2));
+
+        if(radius <= MAX_RADIUS){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
 
-function drawNode(xPos, yPos){
+function drawNode(xPos, yPos, color){
     /*
     Given an (x,y) coordinate, it draws a semitransparent circle on the
     canvas of radius 5 px.
@@ -22,15 +38,43 @@ function drawNode(xPos, yPos){
     INPUT
     xPos: The x coordinate of the node
     yPos: The y coordinate of the node
+    color [optional]: An optional hex string specifying the color of the node.
+    Defaults to #FF0000 (red).
     */
 
     ctx.beginPath();
-    //Make the node circle red and thicken the stroke
+    //Make the node circle red or the user given color and thicken the stroke
     ctx.strokeStyle = '#FF0000';
+    if(color !== undefined) ctx.strokeStyle = color;
+
     ctx.lineWidth = 3;
 
     ctx.arc(xPos, yPos, 10, 0, 2 * Math.PI);
     ctx.stroke();
+}
+
+function drawEdge(node1, node2){
+    /*
+    Takes an input of two node objects and draws an edge between them.
+
+    INPUT
+    node1: The first node
+    node2: The second node
+    */
+
+    ctx.beginPath();
+
+    //Make edges blue and thick
+    ctx.strokeStyle = '#0000ff';
+    ctx.lineWidth = 3;
+
+    ctx.moveTo(node1.xPos, node1.yPos);
+    ctx.lineTo(node2.xPos, node2.yPos);
+    ctx.stroke();
+
+    //Redraw the nodes as red to show that edge adding has finished
+    drawNode(node1.xPos, node1.yPos);
+    drawNode(node2.xPos, node2.yPos);
 }
 
 function reDrawCanvas(){
@@ -42,6 +86,45 @@ function reDrawCanvas(){
 
     for(i = 0; i < nodes.length; i++){
         drawNode(nodes[i].xPos, nodes[i].yPos);
+    }
+}
+
+var selected  = [];
+
+function addEdge(xPos, yPos){
+    /*
+    Takes an (xPos, yPos) coordinate. The function checks whether two nodes have been
+    'selected', and then adds an edge between them.
+
+    INPUT
+    xPos: The x coordinate of the user's click when selecting a node
+    yPos: The y coordinate of the user's click when selecting a node
+    */
+
+    //Find which node the user has selected with his click and redraw it
+    for(i = nodes.length - 1; i >= 0; i--){
+        if(nodes[i].inNode(xPos, yPos)){
+
+            //Add a new selected node and draw it on the screen in green
+            selected.push(i);
+            drawNode(nodes[i].xPos, nodes[i].yPos, '#66ff99');
+            break;
+        }
+    }
+
+    //Check if we have two edges selected and draw an edge
+
+    if(selected.length === 2){
+        //Add an edge and draw it
+
+        var node1Ind = selected[0],
+            node2Ind = selected[1];
+
+        nodes[node1Ind].adjacencies.push({'index': node2Ind, 'weight': 1});
+        nodes[node2Ind].adjacencies.push({'index': node1Ind, 'weight': 1});
+
+        drawEdge(nodes[node1Ind], nodes[node2Ind]);
+        selected = [];
     }
 }
 
@@ -71,19 +154,12 @@ function removeNode(xPos, yPos){
     xPos: The x position of the node
     yPos: The y position of the node
     */
-    
-    //The maximum radius at which a point is still in the circle
-    var MAX_RADIUS = 10;
 
     //Check each node and check to see the radius at which the click is from it
-    for(i = 0; i<nodes.length; i++){
-        //Apply pythagorean theorem
-        var radius = Math.sqrt(Math.pow(xPos - nodes[i].xPos, 2) + 
-        Math.pow(yPos - nodes[i].yPos, 2));
-
+    for(i = nodes.length - 1; i>=0; i--){
         //If the radius is less than the maximum radius of the circle then
         //our click is in the node
-        if(radius <= MAX_RADIUS){
+        if(nodes[i].inNode(xPos, yPos)){
             
             //Remove the entry from the node array and clear it's circle
             ctx.clearRect(nodes[i].xPos, nodes[i].yPos, 20, 20);
@@ -121,6 +197,7 @@ function canvasClick(e){
             break;
         
         case 'adding edges':
+            addEdge(xPos, yPos);
             break;
 
         default:
